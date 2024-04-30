fb4816950c3f4a0e989554de56b46f8527c2f936
tinkugupta5 <tinku.gupta4@gmail.com>
2024-04-30 23:27:57 +0530

    changes in the controller files

---
+++ routes/testRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+//router obj
+
+const router = express.Router();
+
+//routes
+
+router.get('/test',)
+
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+// rest object 
+const app = express();
+
+//routes
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to bloodddd bank app'
+    })
+})
+
+// http://localhost:8080
+
+//port
+const PORT = 8080
+
+// listen
+
+app.listen(PORT, () => {
+
+})
