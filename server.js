05b1ffbe3b8473a917ac999910332dfa8a765343
tinkugupta5 <tinku.gupta4@gmail.com>
2024-05-17 23:50:50 +0530

    chnages the messages

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 //routes
 app.get('/', (req, res) => {
     res.status(200).json({
-        message: 'Welcome to bloodddd bank app'
+        message: 'Welcome to blood bank app'
     })
 })
 // http://localhost:8080
